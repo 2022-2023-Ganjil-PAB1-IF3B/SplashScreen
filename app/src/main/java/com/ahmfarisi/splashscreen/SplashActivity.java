881233d3116e4880a3f61c834e62df8c52272c28
Ahmad Farisi <ahmfarisi@gmail.com>
2022-11-28 11:22:21 +0700

    Finish Project

--- app/src/main/java/com/ahmfarisi/splashscreen/SplashActivity.java
+++ app/src/main/java/com/ahmfarisi/splashscreen/SplashActivity.java
@@ -2,7 +2,10 @@ package com.ahmfarisi.splashscreen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Handler HDL = new Handler();
+        HDL.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 2000);
+
     }
 }
